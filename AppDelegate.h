f5988aa9bc0c610c1c8f21bd95363381f71ba338
Lukas Pitschl <lukas@dressyvagabonds.com>
2012-09-27 00:23:13 +0200

    Keep pinentry compatible with 10.5 as well.

--- AppDelegate.h
+++ AppDelegate.h
@@ -1,5 +1,9 @@
 #import <Cocoa/Cocoa.h>
 
+#if (MAC_OS_X_VERSION_MAX_ALLOWED <= MAC_OS_X_VERSION_10_5)
+@interface AppDelegate : NSObject
+#else
 @interface AppDelegate : NSObject <NSApplicationDelegate>
+#endif
 
 @end
