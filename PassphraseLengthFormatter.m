98d883de855ca543cf6989665527e9cabfa6ebd4
Lukas Pitschl <lukas@dressyvagabonds.com>
2014-12-27 04:21:23 +0100

    [FIX] Correctly display the entered passphrase when "Show typing" is enabled [#145 state:fixed]
    - Create new strings in PassphraseLengthFormatter in order to make sure that
      bindings are correctly updated. Otherwise they won't be, which led to this bug
      in the first place.

--- PassphraseLengthFormatter.m
+++ PassphraseLengthFormatter.m
@@ -1,23 +1,32 @@
 #import "PassphraseLengthFormatter.h"
 #import <Cocoa/Cocoa.h>
 
+#define MAX_PASSPHRASE_LENGTH 300
+
 @implementation PassphraseLengthFormatter
 
 - (NSString *)stringForObjectValue:(id)object {
-	return object;
+    if(![object isKindOfClass:[NSString class]])
+        return nil;
+
+    return [NSString stringWithString:object];
 }
 
 - (BOOL)getObjectValue:(id *)object forString:(NSString *)string errorDescription:(NSString **)error {
-	*object = string;
+	// Generate a new string here, otherwise bindings won't work properly.
+    *object = [NSString stringWithString:string];
 	return YES;
 }
 
-- (BOOL)isPartialStringValid:(NSString *)partialString newEditingString:(NSString **)newString errorDescription:(NSString **)error {
-	if (partialString.length > 300) {
-		*newString = [partialString substringToIndex:300];
-		return NO;
-	}
-	return YES;
+- (BOOL)isPartialStringValid:(NSString **)partialStringPtr proposedSelectedRange:(NSRangePointer)proposedSelRangePtr originalString:(NSString *)origString originalSelectedRange:(NSRange)origSelRange errorDescription:(NSString **)error {
+    // Code found on http://stackoverflow.com/a/19635242 which seems to work properly and as expected.
+
+    NSString *proposedString = *partialStringPtr;
+    if([proposedString length] <= MAX_PASSPHRASE_LENGTH)
+        return YES;
+
+    *partialStringPtr = [NSString stringWithString:[proposedString substringToIndex:MAX_PASSPHRASE_LENGTH]];
+    return NO;
 }
 
 @end
